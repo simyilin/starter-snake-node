f0c9c49a3b9c06714fb9fe560696f584285806d6
Sim Yi Lin <simyilin@umich.edu>
2021-01-14 01:24:13 +0800

    Customized snake

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function handleIndex(request, response) {
   var battlesnakeInfo = {
     apiversion: '1',
     author: '',
-    color: '#888888',
-    head: 'default',
-    tail: 'default'
+    color: '#3E338F',
+    head: 'beluga',
+    tail: 'bolt'
   }
   response.status(200).json(battlesnakeInfo)
 }
