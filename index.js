24c2289134a41125e09c205d611ed513969a4d05
Sim Yi Lin <simyilin@umich.edu>
2021-01-14 01:10:38 +0800

    Avoid wall and snake collisions

--- index.js
+++ index.js
@@ -32,11 +32,78 @@ function handleStart(request, response) {
   response.status(200).send('ok')
 }
 
+// Functions for battlesnake strategy
+// Checks if move would hit the wall
+function hitsWall(newHeadPos, boardWidth, boardHeight) {
+  if (newHeadPos.y >= boardHeight || newHeadPos.y < 0 || newHeadPos.x >= boardWidth || newHeadPos.x < 0) {
+    console.log("Hits wall")
+    return true
+  }
+  return false
+}
+
+// Checks if move would hit self
+function hitsSnake(newHeadPos, bodyCoords) {
+  for (var i = 0; i < bodyCoords.length; ++i) {
+    var bodyCoord = bodyCoords[i]
+    if (newHeadPos.x === bodyCoord.x && newHeadPos.y === bodyCoord.y) {
+      console.log("Hits snake")
+      return true
+    }
+  }
+  return false
+}
+
+// Checks if move would hit other snakes
+function hitsOtherSnakes(newHeadPos, snakes) {
+  for (var i = 0; i < snakes.length; ++i) {
+    var snake = snakes[i]
+    if (hitsSnake(newHeadPos, snake.body)) {
+      return true
+    }
+  }
+  return false
+}
+
+// Returns object with new head position after move
+function getNewPos(headCoord, move) {
+  var newPos = {...headCoord}
+  switch (move) {
+    case 'up':
+      newPos.y = headCoord.y + 1
+      break
+    case 'down':
+      newPos.y = headCoord.y - 1
+      break
+    case 'left':
+      newPos.x = headCoord.x - 1
+      break
+    case 'right':
+      newPos.x = headCoord.x + 1
+      break
+  }
+  return newPos
+}
+
 function handleMove(request, response) {
   var gameData = request.body
 
+  var yourSnake = gameData.you
+  var board = gameData.board
+
   var possibleMoves = ['up', 'down', 'left', 'right']
-  var move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
+  var i = 0
+  while (i < 4) {
+    var move = possibleMoves[i]
+    var newHeadPos = getNewPos(yourSnake.head, move)
+    headHitsWall = hitsWall(newHeadPos, board.width, board.height)
+    var headHitsSelf = hitsSnake(newHeadPos, yourSnake.body)
+    var headHitsSnake = hitsOtherSnakes(newHeadPos, board.snakes)
+    if (!headHitsWall && !headHitsSelf && !headHitsSnake) {
+      break
+    }
+    ++i
+  }
 
   console.log('MOVE: ' + move)
   response.status(200).send({
